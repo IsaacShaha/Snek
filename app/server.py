7fd249fd9996532c4391e45567a5255a9a89a32b
Isaac Shaha <isaac.shaha64@gmail.com>
2020-03-08 23:30:20 -0700

    Snake chases food.

--- app/server.py
+++ app/server.py
@@ -23,158 +23,150 @@ def printGrid(grid):
 
 @bottle.route("/")
 def index():
-    return "Your Battlesnake is alive!"
+	return "Your Battlesnake is alive!"
 
 @bottle.post("/ping")
 def ping():
-    """
-    Used by the Battlesnake Engine to make sure your snake is still working.
-    """
-    return HTTPResponse(status=200)
+	"""
+	Used by the Battlesnake Engine to make sure your snake is still working.
+	"""
+	return HTTPResponse(status=200)
 
 @bottle.post("/start")
 def start():
-    """
-    Called every time a new Battlesnake game starts and your snake is in it.
-    Your response will control how your snake is displayed on the board.
-    """
-    data = bottle.request.json
-    #print("START:", json.dumps(data))
+	"""
+	Called every time a new Battlesnake game starts and your snake is in it.
+	Your response will control how your snake is displayed on the board.
+	"""
+	data = bottle.request.json
+	#print("START:", json.dumps(data))
 
-    response = {"color": "#91E175", "headType": "bwc-ski", "tailType": "skinny"}
-    return HTTPResponse(
-        status=200,
-        headers={"Content-Type": "application/json"},
-        body=json.dumps(response),
-    )
+	response = {"color": "#91E175", "headType": "bwc-ski", "tailType": "skinny"}
+	return HTTPResponse(
+		status=200,
+		headers={"Content-Type": "application/json"},
+		body=json.dumps(response),
+	)
 
 
 @bottle.post("/move")
 def move():
-    """
-    Called when the Battlesnake Engine needs to know your next move.
-    The data parameter will contain information about the board.
-    Your response must include your move of up, down, left, or right.
-    """
+	"""
+	Called when the Battlesnake Engine needs to know your next move.
+	The data parameter will contain information about the board.
+	Your response must include your move of up, down, left, or right.
+	"""
 
-    global turnNumber
-    global direction
-    global priority
+	global turnNumber
+	global direction
+	global priority
 
-    data = bottle.request.json
-    turnNumber += 1
-    #print("\nMOVE " + str(turnNumber) + ":", json.dumps(data))
+	data = bottle.request.json
+	turnNumber += 1
+	#print("\nMOVE " + str(turnNumber) + ":", json.dumps(data))
 
-    grid = []
-    #Create grid, initialize with -1 on each spot.
-    for i in range(data["board"]["height"]):
-    	grid.append([])
-    	for j in range(data["board"]["width"]):
-    		grid[i].append(-1)
-    #Add food to grid, as 0.
-    for food in data["board"]["food"]:
-    	grid[food["y"]][food["x"]] = 0
-    #Add snake parts to grid, as "h" for head and "b" for body and tail.
-    for snake in data["board"]["snakes"]:
-    	for i in range(len(snake["body"])):
-    		body = snake["body"][i]
-    		grid[body["y"]][body["x"]] = "h" if i == 0 else "b"
+	grid = []
+	#Create grid, initialize with -1 on each spot.
+	for i in range(data["board"]["height"]):
+		grid.append([])
+		for j in range(data["board"]["width"]):
+			grid[i].append(-1)
+	#Add food to grid, as 0.
+	for food in data["board"]["food"]:
+		grid[food["y"]][food["x"]] = 0
+	#Add snake parts to grid, as "h" for head and "b" for body and tail.
+	for snake in data["board"]["snakes"]:
+		for i in range(len(snake["body"])):
+			body = snake["body"][i]
+			grid[body["y"]][body["x"]] = "b" if i != 0 and grid[body["y"]][body["x"]] != "h" else "h"
 
-    #Replace -1's with distance to nearest food.
-    changed = True
-    while changed:
-    	changed = False
-    	for y in range(len(grid)):
-    		for x in range(len(grid)):
-    			if not isinstance(grid[y][x], int):
-    				continue
-    			#Add neighbouring grids that exist to d, differential list.
-    			differential = []
-    			if y > 0:
-    				differential.append((-1, 0))
-    			if y < data["board"]["height"] - 1:
-    				differential.append((1, 0))
-    			if x > 0:
-    				differential.append((0, -1))
-    			if x < data["board"]["width"] - 1:
-    				differential.append((0, 1))
-    			#Condition 1: I am -1 and my neighbour is 0. Change to 1.
-    			#Condition 2: I am > 1 and my neighbour is 0. Change to 1.
-				#Condition 3: I am -1 and my neighbour is any positive integer. Change to neighbour+1.
-				#Condition 4: I am > 2 and my neighbour is any positive integer I am 2 or more
-				#				greater above my neighbour. Change to neighbour+1.
-    			for d in differential:
-    				neighbour = grid[y+d[0]][x+d[1]]
-    				if not isinstance(neighbour, int):
-    					continue
-    				#Condition 1
-    				if grid[y][x] == -1 and neighbour == 0:
-    					grid[y][x] = 1
-    					changed = True
+	#Replace -1's with distance to nearest food.
+	changed = True
+	while changed:
+		changed = False
+		for y in range(len(grid)):
+			for x in range(len(grid)):
+				if not isinstance(grid[y][x], int):
+					continue
+				#Add neighbouring grids that exist to d, differential list.
+				differential = []
+				if y > 0:
+					differential.append((-1, 0))
+				if y < data["board"]["height"] - 1:
+					differential.append((1, 0))
+				if x > 0:
+					differential.append((0, -1))
+				if x < data["board"]["width"] - 1:
+					differential.append((0, 1))
+				#Condition 1: I am -1 and my neighbour is >= 0. Change to neighbour+1.
+				#Condition 2: My neighbour is >= 0 and I am 2 or more greater than my neighbour. Change to neighbour+1.
+				for d in differential:
+					neighbour = grid[y+d[0]][x+d[1]]
+					if not isinstance(neighbour, int):
+						continue
+					#Condition 1
+					if grid[y][x] == -1 and neighbour >= 0:
+						grid[y][x] = neighbour + 1
+						changed = True
 					#Condition 2
-    				elif grid[y][x] > 1 and neighbour == 0:
-    					grid[y][x] = 1
-    					changed = True
-    				#Condition 3
-    				elif grid[y][x] == -1 and neighbour > 0:
-    					grid[y][x] = neighbour + 1
-    					changed = True
-    				#Condition 4
-    				elif grid[y][x] > 2 and neighbour > 0 and grid[y][x] - neighbour > 1:
-    					grid[y][x] = neighbour + 1
-    					changed = True
-    printGrid(grid)
+					elif neighbour >= 0 and grid[y][x] - neighbour >= 2:
+						grid[y][x] = neighbour + 1
+						changed = True
 
-    head = data["you"]["body"][0]
+	head = data["you"]["body"][0]
+	moves = ((head["x"], head["y"]-1, "up"), (head["x"]-1, head["y"], "left"),
+		(head["x"]+1, head["y"], "right"), (head["x"], head["y"]+1, "down"))
 
-    food = data["board"]["food"]
-    closestFood = -1
-    closestDistance = -1
-    for i in range(len(food)):
-    	distance = getDistance(food[i], head)
-    	if closestFood == -1 or distance < closestDistance:
-    		closestFood = food[i]
-    		closestDistance = distance
+	validMoves = []
+	for move in moves:
+		if move[0] > 0 and move[0] < data["board"]["width"] - 1 and move[1] > 0 and move[1] < data["board"]["height"] - 1:
+			validMoves.append(move)
 
-    # Choose a random direction to move in
-    directions = ["up", "right", "down", "left"]
-    direction = (direction + 1)%4
-    move = directions[direction]
+	lowestNumber = -1
+	move = "up"
+	#Condition 1: lowestNumber is -1 and neighbour is a nonnegative number.
+	#Condition 2: lowestNumber is >= 0 and neighbour is < lowestNumber.
+	for validMove in validMoves:
+		neighbour = grid[validMove[1]][validMove[0]]
+		if not isinstance(neighbour, int):
+			continue
+		if (lowestNumber == -1 and neighbour >= 0) or (lowestNumber >= 0 and neighbour < lowestNumber):
+			lowestNumber = neighbour
+			move = validMove[2]
 
-    # Shouts are messages sent to all the other snakes in the game.
-    # Shouts are not displayed on the game board.
-    shout = "am snek :-) pls 2 meet u"
+	# Shouts are messages sent to all the other snakes in the game.
+	# Shouts are not displayed on the game board.
+	shout = "am snek :-) pls 2 meet u"
 
-    response = {"move": move, "shout": shout}
-    '''
-    return HTTPResponse(
-    	status=200,
-    	headers={"Content-Type": "application/json"},
-    	body=json.dumps(response),
-    )
-    '''
+	response = {"move": move, "shout": shout}
+
+	return HTTPResponse(
+		status=200,
+		headers={"Content-Type": "application/json"},
+		body=json.dumps(response),
+	)
 
 @bottle.post("/end")
 def end():
-    """
-    Called every time a game with your snake in it ends.
-    """
-    data = bottle.request.json
-    #print("END:", json.dumps(data))
-    return HTTPResponse(status=200)
+	"""
+	Called every time a game with your snake in it ends.
+	"""
+	data = bottle.request.json
+	#print("END:", json.dumps(data))
+	return HTTPResponse(status=200)
 
 
 def main():
-    bottle.run(
-        application,
-        host=os.getenv("IP", "0.0.0.0"),
-        port=os.getenv("PORT", "8080"),
-        debug=os.getenv("DEBUG", True),
-    )
+	bottle.run(
+		application,
+		host=os.getenv("IP", "0.0.0.0"),
+		port=os.getenv("PORT", "8080"),
+		debug=os.getenv("DEBUG", True),
+	)
 
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
 if __name__ == "__main__":
-    main()
+	main()
